1d811a5e41d8277defde2ce20901c7a27ea5fe0a
Samuel Davila <samuelrdavila@gmail.com>
2020-10-14 16:58:12 -0500

    updated commenting

--- acquire.py
+++ acquire.py
@@ -15,7 +15,7 @@ def get_zillow_data():
     Function connects to the data science database and returns a data frame containing Zillow data for 2017 properties and predictions. 
     """
     # create SQL query string
-    sql_query = "SELECT * FROM properties_2017 JOIN predictions_2017 on predictions_2017.parcelid = properties_2017.parcelid WHERE unitcnt = 1 AND transactiondate BETWEEN '2017-05-01' AND '2017-06-30'"
+    sql_query = "SELECT * FROM properties_2017 JOIN predictions_2017 on predictions_2017.parcelid = properties_2017.parcelid WHERE unitcnt = 1"
     
     # creates dataframe using data from DS database, Zillow table
     df = pd.read_sql(sql_query, get_connection('zillow'))
