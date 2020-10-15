2dc8175efcf2268fc2ab146310adcf73020b5b43
Samuel Davila <samuelrdavila@gmail.com>
2020-10-15 09:51:36 -0500

    updated docstring

--- prep.py
+++ prep.py
@@ -65,3 +65,23 @@ def zillow_dummy(df):
     # returning df
     return df
 
+def split_data(df, target):
+    """
+    Accepts DF and target variable name. Returns data split into 6 dataframes: y_train, y_validate, y_test, X_train, X_validate, X_test.
+    """
+    # splitting data
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+
+    # specifying which columns to keep in outputted dataframe
+    # x = features | y = target variable
+    X_train = train.drop(columns=[target])
+    y_train = train[[target]]
+    
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[[target]]
+    
+    X_test = test.drop(columns=[target])
+    y_test = test[[target]]
+    
+    return y_train, y_validate, y_test, X_train, X_validate, X_test
